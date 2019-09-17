e632564a289091ebd65d6bed9acdd9aed0a5132b
Murphy McCauley <murphy.mccauley@gmail.com>
2019-09-17 01:10:29 -0700

    Add interactive (REPL) mode

--- takirun.cpp
+++ takirun.cpp
@@ -7,31 +7,80 @@ int main (int argc, char * argv[])
 {
   if (argc < 2)
   {
-    std::cout << "Put the name of a Taki script on the commandline.\n";
+    std::cout << "Put the name of a Taki script on the commandline or -i for interactive.\n";
     return 1;
   }
-  FILE * f = fopen(argv[1], "r");
-  fseek(f, 0, SEEK_END);
-  int length = ftell(f);
-  fseek(f, 0, SEEK_SET);
-  std::string s("\0", length);
-  fread((char *)s.c_str(), 1, length, f); // Bad!
-  fclose(f);
 
+  // Set up interpreter and add commandline arg info to it
   Taki t;
-
   t.global->set_var("argc", std::to_string(argc-1));
   for (int i = 1; i < argc; i++)
   {
     t.global->set_var(std::string("argv") + std::to_string(i-1), argv[i]);
   }
 
-  try
+
+  if (std::string("-i") == argv[1])
   {
-    t.run(s);
+    std::cout << "Taki interactive mode." << std::endl;
+    std::cout << "To enter multiple lines, end a line with a backslash." << std::endl;
+    while (std::cin)
+    {
+      std::string prog = "";
+      std::cout << ">>> ";
+      while (std::cin)
+      {
+        std::string line;
+        std::getline(std::cin, line);
+        if (!std::cin) break;
+        if (line.size() && line[line.size()-1] == '\\')
+        {
+          line = line.substr(0, line.size()-1) + "\n";
+          prog += line;
+          std::cout << "... ";
+          continue;
+        }
+        prog += line;
+        break;
+      }
+      if (prog.size())
+      {
+        try
+        {
+          t.run(prog);
+        }
+        catch (const char * s)
+        {
+          std::cout << "Error: " << s << std::endl;
+        }
+        catch (ReturnException e)
+        {
+          std::cout << "--> " << e.rv << std::endl;
+        }
+      }
+    }
+    std::cout << std::endl << "Bye!" << std::endl;
   }
-  catch (const char * s)
+  else
   {
-    std::cout << "Error: " << s << std::endl;
+    FILE * f = fopen(argv[1], "r");
+    fseek(f, 0, SEEK_END);
+    int length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    std::string s("\0", length);
+    fread((char *)s.c_str(), 1, length, f); // Bad!
+    fclose(f);
+
+    try
+    {
+      t.run(s);
+    }
+    catch (const char * s)
+    {
+      std::cout << "Error: " << s << std::endl;
+      return 1;
+    }
   }
+
+  return 0;
 }
